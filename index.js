05e2fe4aca10e55f1e971905ae9efc5a0c0b4e15
Carlos <piveta_97@hotmail.com>
2022-10-22 17:19:25 -0300

    Adicionando requisição para API

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+const axios = require("axios")
 const express = require("express")
 const app = express()
 
 // configuração
 app.use(express.json())
 app.listen(3000, () => {
-    console.log("servidor online")
+    console.log("servidor online")  
+})
+const api = axios.create({ 
+    baseUrl: 'https://viacep.com.br/ws' 
 })
 
-app.get("/consulta/:cep/cep", (request,response) => {
+app.get('/', (request,response) => {
+    response.send('Hello world')    
+})
+
+app.get("/consulta/:cep/cep", async (request,response) => {
     let cep = request.params.cep
-    response.send(cep)
+    let endereco = await buscaPorCep(cep) 
+    response.send(endereco)
 }) 
+
+const buscaPorCep = async (cep) => {
+    let url = `https://viacep.com.br/ws/${cep}/json/`
+    //console.log(url)
+    let response =  await axios({method:'get',url})
+    //console.log(response.data)
+    let endereco = response.data 
+    
+    return endereco
+}
+
